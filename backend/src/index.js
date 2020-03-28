1d99f406e7b7f75fe37711506ab97a181063d2c2
Gabriel CM <gabriel.moura@gsw.com.br>
2020-03-27 22:49:34 -0300

    refactor: changed server to App.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const routes = require("./routes");
 
-const server = express();
+const app = require("./app");
 
 // Informa o express que iremos utilizar JSON
-server.use(express.json());
-server.use(routes);
-server.use(cors());
-server.listen(3333);
+app.use(express.json());
 
-/**
- * Rota / Recurso
- * recurso -> Ex: /users
- */
-
-/**
- * Métodos HTTP
- * GET:  busca informações no backend
- * POST: cadastra informações no backend
- * PUT: atualiza/altera dados do backend
- * DELETE: deleta algo do backend
- */
-
-/**
- * Tipos de parâmetros:
- *
- * Query Params: Parâmetros nomeados enviados na rota após "?"(Filtros, Paginação, etc).  Ex. /users?name=Gabriel
- * Route Params: Parâmetros utilizados para identificar recursos. Ex: /users/:id
- * Request Body: Corpo da requisição usado para Criar ou Alterar recursos
- * request - guarda todos os dados que vem através da requisição
- * response - retorna uma resposta pro usuario
- */
-
-/**
- * Bancos de dados
- * SQL: MySql, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * NoSQL: MongoDB, CouchDB, etc.
- */
-/**
- * Driver: SELECT * FROM users
- * Query Builder: table('users').select('*').where()
- */
+app.listen(3333);
